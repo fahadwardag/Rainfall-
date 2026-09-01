9759bee3d071457fe798812c158bd5c42c64efb5
fahadwardag <shahfahadwardag@gmail.com>
2026-09-02 00:56:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import streamlit as 
+import streamlit as st 
 import joblib
 
 # Safe import for XGBoost
